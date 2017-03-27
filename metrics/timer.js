b5288773d7ba5b85aec7ad8a1c38dff9615b72f3
osmeteor <meteorsoftware@qq.com>
2017-03-27 17:26:07 +0800

    v 0.0.5

--- metrics/timer.js
+++ metrics/timer.js
@@ -17,39 +17,16 @@ Timer.prototype.update = function(duration) {
   this.meter.mark();
 }
 Timer.prototype.updateInit = function(duration,rate) {
-  // this.histogram.update(duration);
   this.histogram.sample.values.content=duration.sample.values.content;
   this.histogram.min = duration.min;
   this.histogram.max = duration.max;;
   this.histogram.sum =duration.sum;
-  // this.histogram.variance =duration.variance;
-  // this.histogram.mean =duration.mean;
-  // this.histogram.std_dev =duration.std_dev;
   this.histogram.varianceM = duration.variance;
   this.histogram.varianceS = duration.variance;
   this.histogram.count = duration.count;
   this.histogram.median =duration.median;
    // this.meter.startTime = (new Date(1997,0,0)).getTime();
-    // this.meter.count = rate.count-1;
-    //  this.meter.m1Rate.uncounted = rate.count;
-    // this.meter.mark(rate.count);
-  // this.meter.m1Rate.currentRate = rate.m1*1000;
-  // this.meter.m1Rate.uncounted = rate.m1;
-  // this.meter.m1Rate.currentRate = rate.m1;
-  // this.meter.m5Rate.currentRate = rate.m5*1000;
-  // this.meter.m15Rate.currentRate = rate.m15*1000;
-  // this.meter.mark(this.meter.count);
-  // this.meter.m5 = rate.m5;
-  // this.meter.m15 = rate.m15;
-  // this.meter.mean = rate.mean
-  //  this.meter.mark(this.meter.count);
-  // this.meter.mark(rate.count);
-  // this.meter.m1Rate = rate.m1;
-  // this.meter.m5Rate = rate.m5;
-  // this.meter.m15Rate = rate.m15;
-  // this.meter.count = rate.count;;
-  // this.meter.startTime = (new Date).getTime();
-
+   this.meter.count = rate.count;
 }
 
 // delegate these to histogram
